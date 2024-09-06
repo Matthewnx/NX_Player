5fa9f15736dd1dea3f9951ac0f709eb7c67cebd6
Matthewnx <ghmatin100@gmail.com>
2024-09-06 19:42:29 +0330

    setup  classes

--- NXPlayer/script.js
+++ NXPlayer/script.js
@@ -1,210 +1,236 @@
 // create custom HTML tag
 
-const NXplayer = document.createElement('nx-player');
-
-
-// button identify
-const playbtn = document.getElementById('playbtn');
-const prevbtn = document.getElementById('prev');
-const nextbtn = document.getElementById('next');
-const repeatbtn = document.getElementById('repeat');
-const audioimage = document.getElementById('audioimg');
-const audiotitle = document.getElementById('title');
-const audioauthor = document.getElementById('author');
-const progress = document.getElementById('progressor');
-const durationtext = document.getElementById("durationaudio");
-const volumeinput = document.getElementById("volinput");
-const volumeicon = document.querySelectorAll("#volicon");
-const volumebutton = document.getElementById("volumebtn");
-const volumemodal = document.getElementById("volcontiners");
-const progressback = document.getElementById("pbg");
-const progressbackvol = document.getElementById("pbgvolume");
-
-// audio core import
-const audio = new Audio();
-
-// variable here
-let audios = 
-[
+class Nxplayertemplater extends HTMLElement
+{
+    constructor() 
     {
-        image:'music-src/heart.PNG',
-        title:'down town glow',
-        file:'music-src/downtown.mp3',
-        author:'Ghostrifter Official'
-    },
-    {
-        image:'music-src/heart.PNG',
-        title:'down town glow',
-        file:'music-src/downtown.mp3',
-        author:'Ghostrifter Official'
-    },
-    {
-        image:'music-src/heart.PNG',
-        title:'down town glow',
-        file:'music-src/downtown.mp3',
-        author:'Ghostrifter Official'
+        super();
+        this.attachShadow({mode: "open"});
     }
-]
-let playing = false;
-let repeat = false;
-let audioindex = 0;
-let volboolean = false;
-
-//set music data
-function currentplayer()
-{
-    let currentaudio = audios[audioindex];
-    audio.src = currentaudio.file;
-    audioimage.setAttribute('src',currentaudio.image);
-    audiotitle.innerText = currentaudio.title;
-    audioauthor.innerText = currentaudio.author;
-    
-}
-
-// play and pasue functional
-function playaudio()
-{
-    audio.play();
-    playing = true;
-}
-function audiopause() 
-{
-    audio.pause();
-    playing = false;
-}
-function audiocontroler() 
-{
-   playing == false ? playaudio(): audiopause(); 
-}
-
-//previous and next button functional 
-function prevaudio() 
-{
-    audioindex--;
-    if(audioindex < 0)
+    connectedCallback()
     {
-        audioindex = audios.length - 1;
+        const template = document.createElement('nx-player');
+        template.innerHTML = ``
     }
-    currentplayer();
-    playaudio();
 }
-function nextaudio() 
+
+// const NXplayer = document.querySelector('nx-player');
+
+
+fetch('Player.html')
+    .then(response => response.text())
+    .then(content => setup(content));
+
+
+function setup(content) 
 {
-    audioindex++;
-    if(audioindex >= audios.length)
+    // NXplayer.innerHTML = content;
+    // let player = new NXplayer();
+    // document.body.appendChild(player);
+    // button identify
+    const playbtn = document.getElementById('playbtn');
+    const prevbtn = document.getElementById('prev');
+    const nextbtn = document.getElementById('next');
+    const repeatbtn = document.getElementById('repeat');
+    const audioimage = document.getElementById('audioimg');
+    const audiotitle = document.getElementById('title');
+    const audioauthor = document.getElementById('author');
+    const progress = document.getElementById('progressor');
+    const durationtext = document.getElementById("durationaudio");
+    const volumeinput = document.getElementById("volinput");
+    const volumeicon = document.querySelectorAll("#volicon");
+    const volumebutton = document.getElementById("volumebtn");
+    const volumemodal = document.getElementById("volcontiners");
+    const progressback = document.getElementById("pbg");
+    const progressbackvol = document.getElementById("pbgvolume");
+
+    // audio core import
+    const audio = new Audio();
+
+    // variable here
+    let audios = 
+    [
+        {
+            image:'music-src/heart.PNG',
+            title:'down town glow',
+            file:'music-src/downtown.mp3',
+            author:'Ghostrifter Official'
+        },
+        {
+            image:'music-src/heart.PNG',
+            title:'down town glow',
+            file:'music-src/downtown.mp3',
+            author:'Ghostrifter Official'
+        },
+        {
+            image:'music-src/heart.PNG',
+            title:'down town glow',
+            file:'music-src/downtown.mp3',
+            author:'Ghostrifter Official'
+        }
+    ]
+    let playing = false;
+    let repeat = false;
+    let audioindex = 0;
+    let volboolean = false;
+
+    //set music data
+    function currentplayer()
     {
-        audioindex = 0;
+        let currentaudio = audios[audioindex];
+        audio.src = currentaudio.file;
+        audioimage.setAttribute('src',currentaudio.image);
+        audiotitle.innerText = currentaudio.title;
+        audioauthor.innerText = currentaudio.author;
+        
     }
-    currentplayer();
-    playaudio();
-}
-function enablerepeat()
-{
-    repeat = !repeat;
-    console.log(repeat);
-    repeatbtn.classList.toggle('repeatactive');
-}
 
-
-function progressbar()
-{
-    //set time progressor line 
-    let time = audio.currentTime;
-    let duration = audio.duration;
-    let progressvalue = (time / duration) * 100;
-    progress.value = progressvalue;
-
-    //calculate duration times
-    let hours = Math.floor(time / 60);
-    let minutes = Math.floor(time % 60);
-    let durationtime = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
-    //set duration times  
-    durationtext.innerText = durationtime;
-
-    if(time >= duration && repeat)
+    // play and pasue functional
+    function playaudio()
     {
+        audio.play();
+        playing = true;
+    }
+    function audiopause() 
+    {
+        audio.pause();
+        playing = false;
+    }
+    function audiocontroler() 
+    {
+    playing == false ? playaudio(): audiopause(); 
+    }
+
+    //previous and next button functional 
+    function prevaudio() 
+    {
+        audioindex--;
+        if(audioindex < 0)
+        {
+            audioindex = audios.length - 1;
+        }
+        currentplayer();
         playaudio();
     }
-    else if ( time >= duration)
+    function nextaudio() 
     {
-        audiopause();
+        audioindex++;
+        if(audioindex >= audios.length)
+        {
+            audioindex = 0;
+        }
+        currentplayer();
+        playaudio();
     }
-    progressback.style.width = progress.value + "%";
-}
-function updater() 
-{
-    progress.oninput = () => 
+    function enablerepeat()
     {
-        let audiotime = (audio.duration / 100) * progress.value;
-        audio.currentTime = audiotime;
+        repeat = !repeat;
+        console.log(repeat);
+        repeatbtn.classList.toggle('repeatactive');
+    }
+
+
+    function progressbar()
+    {
+        //set time progressor line 
+        let time = audio.currentTime;
+        let duration = audio.duration;
+        let progressvalue = (time / duration) * 100;
+        progress.value = progressvalue;
+
+        //calculate duration times
+        let hours = Math.floor(time / 60);
+        let minutes = Math.floor(time % 60);
+        let durationtime = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
+        //set duration times  
+        durationtext.innerText = durationtime;
+
+        if(time >= duration && repeat)
+        {
+            playaudio();
+        }
+        else if ( time >= duration)
+        {
+            audiopause();
+        }
         progressback.style.width = progress.value + "%";
-        audio.volume = 0;
     }
-    audio.volume = volumeinput.value / 100;
-    progressbar();
-}
-//for fix unhandled background
-progress.oninput = () =>
-{
-    progressback.style.width = progress.value + "%";
-}
-
-
-// volume functional
-volumeinput.oninput = () =>
-{
-    let volumevalue = volumeinput.value;
-    audio.volume = volumevalue / 100;
-    progressbackvol.style.width = volumeinput.value +"%";
-    
-    volumeicon.forEach( icon => {
-        if(volumevalue > 80)
-        {
-            icon.setAttribute('name','volume-high');
-        }
-        if(volumevalue < 80)
-        {
-            icon.setAttribute('name','volume-medium');
-        }
-        if(volumevalue < 50)
-        {
-            icon.setAttribute('name','volume-low');
-        }
-        if(volumevalue < 10)
-        {
-            icon.setAttribute('name','volume-off');
-        }
-        if(volumevalue == 0)
-        {
-            icon.setAttribute('name','volume-mute');
-        }
-    }
-    );
-}
-// fix unhandled background width
-progressbackvol.style.width = "100%";
-
-volumebutton.onclick = () =>
-{
-    if(volboolean == false)
+    function updater() 
     {
-        volumemodal.style.display = "flex";
-        volboolean = true;
+        progress.oninput = () => 
+        {
+            let audiotime = (audio.duration / 100) * progress.value;
+            audio.currentTime = audiotime;
+            progressback.style.width = progress.value + "%";
+            audio.volume = 0;
+        }
+        audio.volume = volumeinput.value / 100;
+        progressbar();
     }
-    else if(volboolean == true)
+    //for fix unhandled background
+    progress.oninput = () =>
     {
-        volumemodal.style.display = "none";
-        volboolean = false;
+        progressback.style.width = progress.value + "%";
     }
-    console.log("hello")
+
+
+    // volume functional
+    volumeinput.oninput = () =>
+    {
+        let volumevalue = volumeinput.value;
+        audio.volume = volumevalue / 100;
+        progressbackvol.style.width = volumeinput.value +"%";
+        
+        volumeicon.forEach( icon => {
+            if(volumevalue > 80)
+            {
+                icon.setAttribute('name','volume-high');
+            }
+            if(volumevalue < 80)
+            {
+                icon.setAttribute('name','volume-medium');
+            }
+            if(volumevalue < 50)
+            {
+                icon.setAttribute('name','volume-low');
+            }
+            if(volumevalue < 10)
+            {
+                icon.setAttribute('name','volume-off');
+            }
+            if(volumevalue == 0)
+            {
+                icon.setAttribute('name','volume-mute');
+            }
+        }
+        );
+    }
+    // fix unhandled background width
+    progressbackvol.style.width = "100%";
+
+    volumebutton.onclick = () =>
+    {
+        if(volboolean == false)
+        {
+            volumemodal.style.display = "flex";
+            volboolean = true;
+        }
+        else if(volboolean == true)
+        {
+            volumemodal.style.display = "none";
+            volboolean = false;
+        }
+        console.log("hello")
+    }
+
+    //add current variable
+    currentplayer();
+
+    //event listner
+    playbtn.addEventListener('click', audiocontroler);
+    prevbtn.addEventListener('click', prevaudio);
+    nextbtn.addEventListener('click', nextaudio);
+    repeatbtn.addEventListener('click', enablerepeat);
+    audio.addEventListener('timeupdate',updater);
+
 }
-
-//add current variable
-currentplayer();
-
-//event listner
-playbtn.addEventListener('click', audiocontroler);
-prevbtn.addEventListener('click', prevaudio);
-nextbtn.addEventListener('click', nextaudio);
-repeatbtn.addEventListener('click', enablerepeat);
-audio.addEventListener('timeupdate',updater);
